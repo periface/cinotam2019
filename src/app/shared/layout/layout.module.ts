41d15daba1e1525cc51c27357359f4c017ee9f5d
Alan <temp_peri@hotmail.com>
2019-07-09 09:50:15 -0500

    footer

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AngularMaterialModule } from '../angular-material.module';
-import { TopMenuComponent } from './top-menu/top-menu.component';
 import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from '../angular-material.module';
+import { FooterComponent } from './footer/footer.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
-  exports: [TopMenuComponent],
-  declarations: [TopMenuComponent],
+  exports: [TopMenuComponent, FooterComponent],
+  declarations: [TopMenuComponent, FooterComponent],
   imports: [AngularMaterialModule, CommonModule, RouterModule]
 })
 export class LayoutModule {}
